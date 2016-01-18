2fb7a1181abe7b47e1be5e056dfc5f22d568e5a7
Cory Kaufman-Schofield <cory@corykaufman.com>
2016-01-17 22:32:51 -0500

    Switch to using ghost npm module

--- core/client/app/session-stores/application.js
+++
@@ -1,6 +0,0 @@
-import AdaptiveStore from 'ember-simple-auth/session-stores/adaptive';
-
-export default AdaptiveStore.extend({
-    localStorageKey: 'ghost:session',
-    cookieName: 'ghost:session'
-});
